32bbb00cd3f66ea983735f743eefe1889be87cc4
fruitfulfuture9 <fruitfulfuture9@gmail.com>
2020-07-04 15:51:48 -0700

    aws code pipe line commit build kick off test

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World V2";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
